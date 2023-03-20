21ec62da24b4b9eaa760560eeef3b56d1aea1b82
dlgustn3768 <dlgustn3768@naver.com>
2023-03-20 14:23:37 +0900

    토글 이벤트 추가

--- Js/todo.js
+++ Js/todo.js
@@ -1,15 +1,34 @@
-
-
-
-
-
-const toggleButton = document.querySelector('.toggle-button');
-const toggleContent = document.querySelector('.toggle-content');
-
-toggleButton.addEventListener('click', function() {
-  if (toggleContent.style.display === 'none') {
-    toggleContent.style.display = 'block';
-  } else {
-    toggleContent.style.display = 'none';
+class TodoEvent{
+  static #instance = null;
+  static getInstance() {
+      if(this.#instance == null) {
+          this.#instance = new TodoEvent();
+      }
+      return this.#instance;
   }
-});
+
+  addEventToggleClick(){
+    const toggleButtons = document.querySelectorAll('.toggle-button');
+    
+    toggleButtons.forEach((toggleButton, index) =>{
+      const toggleContent = document.querySelectorAll('.toggle-content')[index];
+      toggleButton.onclick = () =>{
+        if (toggleContent.style.display === 'none') {
+          toggleContent.style.display = 'block';
+        } else {
+          toggleContent.style.display = 'none';
+        }
+      }
+    });
+  }
+}
+
+class TodoService{
+  static #instance = null;
+  static getInstance() {
+      if(this.#instance == null) {
+          this.#instance = new TodoService();
+      }
+      return this.#instance;
+  }
+}
